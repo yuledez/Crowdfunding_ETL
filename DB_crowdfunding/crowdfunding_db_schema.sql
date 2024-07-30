b22efc9d307c13735b5ba9c5bf9c5abae37377e3
yuledez <yuleica@gmail.com>
2024-07-29 20:08:20 -0700

    updated db_schema

---
+++ DB_crowdfunding/crowdfunding_db_schema.sql
@@ -0,0 +1,70 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(255)   NOT NULL,
+    "description" varchar(255)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledge" float   NOT NULL,
+    "outcome" varchar(30)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(50)   NOT NULL,
+    "currency" varchar(50)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(50)   NOT NULL,
+    "subcategory_id" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "caegory_id" varchar(50)   NOT NULL,
+    "category_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "caegory_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcaegory_id" varchar(50)   NOT NULL,
+    "subcategory_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcaegory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("caegory_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcaegory_id");
+
+--Checking campaign table
+SELECT * FROM campaign
+
+--Checking  table category
+SELECT * FROM category
+
+--Checking  table category
+SELECT * FROM contacts
+
+--Checking  table category
+SELECT * FROM subcategory
